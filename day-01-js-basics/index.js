dbd61fd6bddbee88769d587d292a8173d1175a9b
webwithastha <lodhiastha85@gmail.com>
2025-08-28 19:50:57 +0530

    add day-01 task

---
+++ day-01-js-basics/index.js
@@ -0,0 +1,27 @@
+// day-01-js-basics/index.js
+
+// 1. Variables & types
+let name = "Astha Lodhi";
+let age = 21;
+let isStudent = true;
+
+// 2. Operators
+let nextYearAge = age + 1;
+
+// 3. Student details in an object
+const student = {
+  name: "Astha Lodhi",
+  age: 21,
+  course: "B.Tech in Computer Science",
+  college: "LNCT, Bhopal",
+  isStudent: true,
+};
+
+// 4. Print details
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
+console.log("Is Student:", student.isStudent);
+console.log("Next Year Age:", nextYearAge);
