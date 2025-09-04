aaefb21a13a5ae6e8fd3ce4a99c121fdc0041940
webwithastha <lodhiastha85@gmail.com>
2025-09-04 15:00:00 +0530

    Added morgan for logs

--- day-11/index.js
+++ day-11/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const studentRoutes = require("./routes/studentRoutes");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Mount routes
 app.use("/students", studentRoutes);
